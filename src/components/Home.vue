2bfc3a371c4b0f95df4ac5289a8c0ee1cf65e96d
emiinacio <emilymarcelino59@gmail.com>
2022-04-21 10:44:30 +0100

    update

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+      <div class="pills">
+        <Pill v-for="word in groupedWord" :key="word.name" 
+        :name="word.name" :amount="word.amount"/>
+      </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill.vue";
+
+export default {
+  name: "App",
+  components: { Pill },
+  data: function () {
+    return {
+      groupedWord: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+    .pills {
+        display: flex;
+    }
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <span>{{name}}</span>
+      <span>{{amount}}</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  props: ["name", "amount"],
+};
+</script>
+
+<style>
+</style>
